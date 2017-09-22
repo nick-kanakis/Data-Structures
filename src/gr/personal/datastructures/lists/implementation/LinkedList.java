7c191aabf24a96aef3b1f48f3803c3290f2a9cc7
nkanakis <nicolas.kanakis@gmail.com>
2017-09-22 16:22:29 +0300

    Completed LinkedList Implementation

--- src/gr/personal/datastructures/lists/implementation/LinkedList.java
+++ src/gr/personal/datastructures/lists/implementation/LinkedList.java
@@ -9,9 +9,9 @@ import java.util.List;
  * Linked List API
  * ----------------------------------------------
  *
- *  void    add(Item item)      	    // append an item in the last of list.
  *  boolean isEmpty()                   // returns true if list is empty, false otherwise.
  *  int     size()                      // returns the length of list in integer.
+ *  void    add(Item item)      	    // append an item in the last of list.
  *  void    add(Item item, int index)   //append an item at the given index in the list.
  *  Item    get(int index)              // returns the item at the given index from the list.
  *  boolean remove(int index)	        // remove the node and return true at the given index from the list.
@@ -37,9 +37,27 @@ public class LinkedList<Item> {
         head = new Node();
     }
 
-    //Add new node at the end.
+
+    public boolean isEmpty(){
+        if (head == null || head.value ==null)
+            return true;
+        return false;
+    }
+
+    public int size(){
+        if (head == null || head.value ==null)
+            return 0;
+        Node tempNode = head;
+        int sizeCount = 1;
+        while(tempNode.nextNode !=null){
+            tempNode = tempNode.nextNode;
+            sizeCount++;
+        }
+        return sizeCount;
+    }
+
     public void add(Item value){
-        Node lastNode;
+        Node lastNode = head;
         Node addedNode = new Node(value);
 
         if(head ==null){
@@ -52,17 +70,67 @@ public class LinkedList<Item> {
             return;
         }
 
-        lastNode = returnLastNode(head);
+        while(lastNode.nextNode !=null){
+            lastNode = lastNode.nextNode;
+        }
         lastNode.nextNode = addedNode;
     }
 
-    private Node returnLastNode(Node head){
-        Node tempNode = head;
-        while(tempNode.nextNode !=null){
-            tempNode = tempNode.nextNode;
+    public void add(Item value, int index) {
+        validateIndex(index);
+
+        Node addedNode = new Node(value);
+        Node beforeIndexNode = head;
+
+        if(index==1){
+            addedNode.nextNode = head;
+            head = addedNode;
+            return;
         }
-        return tempNode;
+
+        int currentPositionInList = 1;
+        while(currentPositionInList != index -1){
+            beforeIndexNode = beforeIndexNode.nextNode;
+            currentPositionInList++;
+        }
+
+        addedNode.nextNode = beforeIndexNode.nextNode;
+        beforeIndexNode.nextNode = addedNode;
     }
+
+    public Item get(int index) {
+        validateIndex(index);
+
+        Node currentNode = head;
+        for (int i = 1; i < index ; i++) {
+            currentNode = currentNode.nextNode;
+        }
+        return currentNode.value;
+    }
+
+    public void remove(int index){
+        validateIndex(index);
+
+        if(index == 1)
+            head = head.nextNode;
+
+
+        Node previousNode = head;
+        Node toBeDeletedNode = head;
+        for (int i = 1; i < index ; i++) {
+            previousNode = toBeDeletedNode;
+            toBeDeletedNode = toBeDeletedNode.nextNode;
+        }
+
+        previousNode.nextNode = toBeDeletedNode.nextNode;
+    }
+
+    private void validateIndex(int index){
+        if(index<=0 || index >size())
+            throw new IndexOutOfBoundsException();
+    }
+
+
 
     public List<Item> returnValues(){
         Node tempNode = head;
--- test/lists/LinkedListTest.java
+++ test/lists/LinkedListTest.java
@@ -32,4 +32,171 @@ public class LinkedListTest extends TestCase {
             assertEquals(expectedResults.get(i), results.get(i));
         }
     }
+
+    public void testIsEmpty(){
+        LinkedList<Integer> list = new LinkedList();
+        assertEquals(true, list.isEmpty());
+
+        list.add(1);
+        assertEquals(false, list.isEmpty());
+    }
+
+    public void testSize(){
+        LinkedList<Integer> list = new LinkedList();
+        assertEquals(0, list.size());
+
+        list.add(1);
+        assertEquals(1, list.size());
+
+        list.add(1);
+        assertEquals(2, list.size());
+    }
+
+    public void testAddWithIndex(){
+
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.add(5,3);
+        assertEquals(5, list.size());
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(1);
+        expectedResults.add(2);
+        expectedResults.add(5);
+        expectedResults.add(3);
+        expectedResults.add(4);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+    public void testAddWithIndexToBeginning(){
+
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.add(5,1);
+        assertEquals(5, list.size());
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(5);
+        expectedResults.add(1);
+        expectedResults.add(2);
+        expectedResults.add(3);
+        expectedResults.add(4);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+    public void testAddWithIndexToEnd(){
+
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.add(5,4);
+        assertEquals(5, list.size());
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(1);
+        expectedResults.add(2);
+        expectedResults.add(3);
+        expectedResults.add(5);
+        expectedResults.add(4);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+    public void testGet(){
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        assertEquals(1, list.get(1).intValue());
+        list.add(2);
+        assertEquals(2, list.get(2).intValue());
+        list.add(3);
+        assertEquals(3, list.get(3).intValue());
+        list.add(4);
+        assertEquals(4, list.get(4).intValue());
+    }
+
+
+    public void testRemove(){
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.remove(2);
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(1);
+        expectedResults.add(3);
+        expectedResults.add(4);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+    public void testRemoveBeginning(){
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.remove(1);
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(2);
+        expectedResults.add(3);
+        expectedResults.add(4);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+    public void testRemoveEnd(){
+        LinkedList<Integer> list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.remove(4);
+
+        ArrayList<Integer> expectedResults = new ArrayList();
+        expectedResults.add(1);
+        expectedResults.add(2);
+        expectedResults.add(3);
+
+        List<Integer> results = list.returnValues();
+        for (int i = 0; i < expectedResults.size(); i++) {
+            assertEquals(expectedResults.get(i), results.get(i));
+        }
+    }
+
+
+
 }
