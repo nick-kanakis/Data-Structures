0fb040db07f8132472acf0aade8a6bb2fe6bd167
nkanakis <nicolas.kanakis@gmail.com>
2017-09-27 17:30:28 +0300

    Binary Search Tree implementation

--- src/gr/personal/datastructures/trees/BinarySearchTree.java
+++ src/gr/personal/datastructures/trees/BinarySearchTree.java
@@ -5,7 +5,7 @@ package gr.personal.datastructures.trees;
  */
 public class BinarySearchTree<V> {
 
-    public class Node{
+    public class Node {
         int key;
         V value;
         Node leftChild;
@@ -22,65 +22,210 @@ public class BinarySearchTree<V> {
 
     private Node root;
 
-    public void insert(int key, V value){
+    public void insert(int key, V value) {
 
-        Node toBeInserted = new Node(key,value);
+        Node toBeInserted = new Node(key, value);
 
-        if(root ==null)
+        if (root == null)
             root = toBeInserted;
         else
-            insertRecursively(root,toBeInserted);
+            insertRecursively(root, toBeInserted);
 
     }
 
-    private void insertRecursively(Node currentRoot, Node toBeInserted){
+    private void insertRecursively(Node currentRoot, Node toBeInserted) {
 
-        if(currentRoot.key == toBeInserted.key)
+        if (currentRoot.key == toBeInserted.key)
             return;
 
-        if(currentRoot.key < toBeInserted.key){
+        if (toBeInserted.key < currentRoot.key) {
 
-            if(currentRoot.leftChild == null)
+            if (currentRoot.leftChild == null)
                 currentRoot.leftChild = toBeInserted;
             else
                 insertRecursively(currentRoot.leftChild, toBeInserted);
-        }
-        else {
+        } else {
 
-            if(currentRoot.rightChild == null)
+            if (currentRoot.rightChild == null)
                 currentRoot.rightChild = toBeInserted;
             else
                 insertRecursively(currentRoot.rightChild, toBeInserted);
         }
     }
 
-    public V retrieve(int key){
-         return retrieveRecursively(root, key);
+    public V retrieve(int key) {
+        return retrieveRecursively(root, key);
     }
 
-    private V retrieveRecursively(Node currentRoot, int key){
-        if(currentRoot.key == key)
+    private V retrieveRecursively(Node currentRoot, int key) {
+        if (currentRoot == null)
+            return null;
+
+        if (currentRoot.key == key)
             return currentRoot.value;
-        else if (currentRoot.key< key)
-            retrieveRecursively(currentRoot.leftChild, key);
+        else if (key < currentRoot.key)
+            return retrieveRecursively(currentRoot.leftChild, key);
         else
-            retrieveRecursively(currentRoot.rightChild, key);
+            return retrieveRecursively(currentRoot.rightChild, key);
+    }
+
+    public void delete(int key) {
+
+        if (root == null)
+            return;
+
+        Node parentOfToBeDeletedNode = null;
+        Node toBeDeletedNode = null;
+        boolean isLeftChild = false;
+        if (root.key == key)
+            parentOfToBeDeletedNode = root;
+        else
+            parentOfToBeDeletedNode = findParentOfToBeDeletedNode(root, key);
+
+        //if parentNode is still null that means that there is no node with given key
+        if (parentOfToBeDeletedNode == null)
+            return;
+
+        if (key < parentOfToBeDeletedNode.key) {
+            isLeftChild = true;
+            toBeDeletedNode = parentOfToBeDeletedNode.leftChild;
+        } else {
+            isLeftChild = false;
+            toBeDeletedNode = parentOfToBeDeletedNode.rightChild;
+        }
+        removeNode(parentOfToBeDeletedNode, toBeDeletedNode, isLeftChild);
+    }
+
+    /*
+    * Find the parent of the node to be deleted by probing the left/right children of each node recursively.
+    * */
+    private Node findParentOfToBeDeletedNode(Node currentRoot, int key) {
+        if (key < currentRoot.key && currentRoot.leftChild != null) {
+            if (currentRoot.leftChild.key != key)
+                return findParentOfToBeDeletedNode(currentRoot.leftChild, key);
+            else
+                return currentRoot;
+        } else if (key > currentRoot.key && currentRoot.rightChild != null) {
+            if (currentRoot.rightChild.key != key)
+                return findParentOfToBeDeletedNode(currentRoot.rightChild, key);
+            else
+                return currentRoot;
+        }
         return null;
     }
 
-    public void delete(int key){
+    private void removeNode(Node parentOfToBeDeletedNode, Node toBeDeletedNode, boolean isLeftChild) {
 
+        //Case 1: Delete Node is leaf
+        if (toBeDeletedNode.rightChild == null && toBeDeletedNode.leftChild == null) {
+            if (isLeftChild)
+                parentOfToBeDeletedNode.leftChild = null;
+            else
+                parentOfToBeDeletedNode.rightChild = null;
+
+        }
+        //Case 2: Delete Node has only one child
+        else if (toBeDeletedNode.rightChild == null || toBeDeletedNode.leftChild == null) {
+            //Right child of deleted node is the only not null child
+            if (toBeDeletedNode.rightChild != null) {
+                if (isLeftChild)
+                    parentOfToBeDeletedNode.leftChild = toBeDeletedNode.rightChild;
+                else
+                    parentOfToBeDeletedNode.rightChild = toBeDeletedNode.rightChild;
+            }
+            //Left child of deleted node is the only not null child
+            else {
+                if (isLeftChild)
+                    parentOfToBeDeletedNode.leftChild = toBeDeletedNode.leftChild;
+                else
+                    parentOfToBeDeletedNode.rightChild = toBeDeletedNode.leftChild;
+            }
+
+        }
+        //Case 3: Delete Node has 2 children
+        else {
+            Node rootOfRightSubtree = toBeDeletedNode.rightChild;
+            //Case 3.a: There is NO left most element in RIGHT subtree of the deleted node.
+            if (rootOfRightSubtree.leftChild == null) {
+                toBeDeletedNode.value = rootOfRightSubtree.value;
+                toBeDeletedNode.key = rootOfRightSubtree.key;
+                if (isLeftChild) {
+                    toBeDeletedNode.leftChild = rootOfRightSubtree.rightChild;
+                } else {
+                    toBeDeletedNode.rightChild = rootOfRightSubtree.rightChild;
+                }
+            }
+            //Case 3.b: There is a left most element in RIGHT subtree of the deleted node.
+            else {
+                Node minimumLeafNode = retrieveMinimumValue(rootOfRightSubtree);
+                deleteMinimumLeafOfLeftSubtree(rootOfRightSubtree);
+                toBeDeletedNode.key = minimumLeafNode.key;
+                toBeDeletedNode.value = minimumLeafNode.value;
+            }
+        }
     }
 
 
+    public V retrieveMinimumValue() {
+        return retrieveMinimumValue(root).value;
+    }
 
-    public void printPreOrder(){}
+    private Node retrieveMinimumValue(Node currentRoot) {
+        if (currentRoot == null)  // this check will never be encountered.
+            return null;
+        if (currentRoot.leftChild != null)
+            return retrieveMinimumValue(currentRoot.leftChild);
+        else
+            return currentRoot;
+    }
 
-    public void printInOrder(){}
+    private Node deleteMinimumLeafOfLeftSubtree(Node currentRoot) {
+        if (currentRoot == null)
+            return null;
+        if (currentRoot.leftChild == null)
+            return currentRoot.rightChild;
 
-    public void printPostOrder(){}
+        currentRoot.leftChild = deleteMinimumLeafOfLeftSubtree(currentRoot.leftChild);
+        return currentRoot;
+    }
 
 
+    public void printPreOrder() {
+        printPreOrderRecursively(root);
+    }
 
+    private void printPreOrderRecursively(Node currentRoot) {
+        if (currentRoot != null)
+            System.out.print(currentRoot.value + ",");
 
+        if (currentRoot.leftChild != null)
+            printPreOrderRecursively(currentRoot.leftChild);
+        if (currentRoot.rightChild != null)
+            printPreOrderRecursively(currentRoot.rightChild);
+    }
+
+    public void printInOrder() {
+        printInOrderRecursively(root);
+    }
+
+    private void printInOrderRecursively(Node currentRoot) {
+        if (currentRoot.leftChild != null)
+            printInOrderRecursively(currentRoot.leftChild);
+        System.out.print(currentRoot.value + ",");
+        if (currentRoot.rightChild != null)
+            printInOrderRecursively(currentRoot.rightChild);
+    }
+
+    public void printPostOrder() {
+        printPostOrderRecursively(root);
+    }
+
+    private void printPostOrderRecursively(Node currentRoot) {
+        if (currentRoot.leftChild != null)
+            printPostOrderRecursively(currentRoot.leftChild);
+        if (currentRoot.rightChild != null)
+            printPostOrderRecursively(currentRoot.rightChild);
+        if (currentRoot != null)
+            System.out.print(currentRoot.value + ",");
+    }
 }
---
+++ test/BinarySearchTreeTest.java
@@ -0,0 +1,205 @@
+import gr.personal.datastructures.trees.BinarySearchTree;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
+/**
+ * Created by nkanakis on 9/27/2017.
+ */
+public class BinarySearchTreeTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void testInsert(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+
+
+        assertEquals("10", bst.retrieve(10));
+        assertEquals("20", bst.retrieve(20));
+        assertEquals("5", bst.retrieve(5));
+        assertEquals("15", bst.retrieve(15));
+        assertEquals("14", bst.retrieve(14));
+        assertEquals("25", bst.retrieve(25));
+        assertEquals("6", bst.retrieve(6));
+        assertEquals("3", bst.retrieve(3));
+        assertEquals("16", bst.retrieve(16));
+
+    }
+
+    @Test
+    public void testRetrieveEmptyTree(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+        assertNull( bst.retrieve(10));
+    }
+
+
+    @Test
+    public void testPreOrder(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+
+        bst.printPreOrder();
+        assertEquals("10,5,3,6,20,15,14,16,25,", outContent.toString());
+
+    }
+
+    @Test
+    public void testPostOrder(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+
+        bst.printPostOrder();
+        assertEquals("3,6,5,14,16,15,25,20,10,", outContent.toString());
+
+    }
+
+    @Test
+    public void testInOrder(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+
+        bst.printInOrder();
+        assertEquals("3,5,6,10,14,15,16,20,25,", outContent.toString());
+
+    }
+
+    @Test
+    public void testDeleteLeaf(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+
+        bst.delete(14);
+        bst.printInOrder();
+        assertEquals("3,5,6,10,15,16,20,25,", outContent.toString());
+    }
+
+    @Test
+    public void testDeleteNodeWithOneChild(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(20,"20");
+        bst.insert(5,"5");
+        bst.insert(15,"15");
+        bst.insert(14,"14");
+        bst.insert(25,"25");
+        bst.insert(6,"6");
+        bst.insert(3,"3");
+        bst.insert(16,"16");
+        bst.insert(30,"30");
+
+        bst.delete(25);
+        bst.printInOrder();
+        assertEquals("3,5,6,10,14,15,16,20,30,", outContent.toString());
+    }
+
+    @Test
+    public void testDeleteNodeWithTwoChildWithLeftMostNodes(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(5,"5");
+        bst.insert(50,"50");
+        bst.insert(14,"14");
+        bst.insert(60,"60");
+        bst.insert(65,"65");
+        bst.insert(55,"55");
+        bst.insert(54,"54");
+        bst.insert(58,"58");
+
+
+
+        bst.delete(50);
+        bst.printInOrder();
+        assertEquals("5,10,14,54,55,58,60,65,", outContent.toString());
+
+    }
+
+    @Test
+    public void testDeleteNodeWithTwoChildWithoutLeftMostNodes(){
+        BinarySearchTree<String> bst = new BinarySearchTree<>();
+
+        bst.insert(10,"10");
+        bst.insert(5,"5");
+        bst.insert(50,"50");
+        bst.insert(14,"14");
+        bst.insert(60,"60");
+        bst.insert(65,"65");
+        bst.insert(63,"63");
+        bst.insert(70,"70");
+
+
+        bst.delete(50);
+        bst.printInOrder();
+        assertEquals("5,10,14,60,63,65,70,", outContent.toString());
+
+
+    }
+
+}
