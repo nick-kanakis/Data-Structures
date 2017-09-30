ef86b3a63025cac86ab656b08772630ae42cf54b
nicolasmanic <nicolas.kanakis@gmail.com>
2017-10-01 01:11:11 +0300

    Heap Implementation

---
+++ src/gr/personal/datastructures/heap/Heap.java
@@ -0,0 +1,110 @@
+package gr.personal.datastructures.heap;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by Nicolas on 30/9/2017.
+ *
+ * This is a Max Heap implementation.
+ */
+public class Heap {
+
+    private static final int ORIGINAL_ARRAY_SIZE = 10;
+    private int[] array = new int[ORIGINAL_ARRAY_SIZE];
+    private int lastOccupiedPointer = -1;
+
+    public void insert(int value){
+        if(lastOccupiedPointer <0){
+            array[++lastOccupiedPointer] = value;
+            return;
+        }
+        if( lastOccupiedPointer >= array.length -1  )
+            increaseArray();
+
+        array[++lastOccupiedPointer] = value;
+        heapifyUp(lastOccupiedPointer);
+    }
+
+    public int extractMax(){
+        if(array.length <=0)
+            throw new IllegalStateException();
+
+        int extractedValue = array[0];
+        swapCells(0, lastOccupiedPointer);
+        array[lastOccupiedPointer] = 0;
+        lastOccupiedPointer--;
+        heapifyDown(0);
+        return extractedValue;
+    }
+
+    public int[] retrieveArray(){
+        return array;
+    }
+
+    private void heapifyDown(int currentPointer) {
+        while(currentPointer <= lastOccupiedPointer){
+            int rightChildPointer = calculateRightChildPointer(currentPointer);
+            int leftChildPointer = calculateLeftChildPointer(currentPointer);
+
+            if(rightChildPointer > array.length -1 ||leftChildPointer > array.length -1)
+                break;
+
+            int maxChildPointer;
+            if(array[leftChildPointer] > array[rightChildPointer] )
+                maxChildPointer = leftChildPointer;
+            else
+                maxChildPointer = rightChildPointer;
+
+            if(array[currentPointer] < array[maxChildPointer])
+                swapCells(currentPointer, maxChildPointer);
+            else
+                break;
+
+            currentPointer = maxChildPointer;
+        }
+    }
+
+    private void heapifyUp(int currentPointer) {
+        while(currentPointer > 0){
+            int parentPointer = calculateParentPointer(currentPointer);
+            if(array[parentPointer] < array[currentPointer])
+                swapCells(parentPointer, currentPointer);
+            else
+                break;
+            currentPointer = parentPointer;
+        }
+    }
+
+    private int calculateParentPointer(int currentPointer){
+        if(currentPointer <0)
+            throw new IllegalArgumentException();
+        return (currentPointer - 1)/2;
+    }
+
+    private int calculateRightChildPointer(int currentPointer){
+        if(currentPointer <0)
+            throw new IllegalArgumentException();
+        return (currentPointer*2)+2;
+    }
+
+    private int calculateLeftChildPointer(int currentPointer){
+        if(currentPointer <0)
+            throw new IllegalArgumentException();
+        return (currentPointer*2)+1;
+    }
+
+    private void swapCells(int parentPointer, int currentPointer) {
+        int temp = array[parentPointer];
+        array[parentPointer] = array[currentPointer];
+        array[currentPointer] = temp;
+    }
+
+    private void increaseArray() {
+        int[] clonedArray = new int[array.length*2];
+        for (int i = 0; i < array.length; i++) {
+            clonedArray[i] = array[i];
+        }
+        array = clonedArray;
+        clonedArray = null;
+    }
+}
---
+++ test/heap/HeapTest.java
@@ -0,0 +1,126 @@
+package heap;
+
+import gr.personal.datastructures.heap.Heap;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Nicolas on 30/9/2017.
+ */
+public class HeapTest {
+
+    @Test
+    public void testHeap(){
+
+        Heap heap = new Heap();
+
+        heap.insert(5);
+        int[] correctArray = {5,0,0,0,0,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(3);
+        correctArray = new int[]{5,3,0,0,0,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(2);
+        correctArray = new int[]{5,3,2,0,0,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(10);
+        correctArray = new int[]{10,5,2,3,0,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(4);
+        correctArray = new int[]{10,5,2,3,4,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(8);
+        correctArray = new int[]{10,5,8,3,4,2,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(11);
+        correctArray = new int[]{11,5,10,3,4,2,8,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(1);
+        correctArray = new int[]{11,5,10,3,4,2,8,1,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(15);
+        correctArray = new int[]{15,11,10,5,4,2,8,1,3,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(6);
+        correctArray = new int[]{15,11,10,5,6,2,8,1,3,4};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        heap.insert(7);
+        correctArray = new int[]{15,11,10,5,7,2,8,1,3,4,6,0,0,0,0,0,0,0,0,0};
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+    }
+
+
+    @Test
+    public void testMaxExtraction() throws Exception {
+
+        Heap heap = new Heap();
+
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(2);
+        heap.insert(10);
+        heap.insert(4);
+        heap.insert(8);
+        heap.insert(11);
+        heap.insert(1);
+        heap.insert(15);
+        heap.insert(6);
+
+
+        int[] correctArray = new int[]{11,6,10,5,4,2,8,1,3,0};
+        assertEquals(15, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{10,6,8,5,4,2,3,1,0,0};
+        assertEquals(11, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{8,6,3,5,4,2,1,0,0,0};
+        assertEquals(10, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{6,5,3,1,4,2,0,0,0,0};
+        assertEquals(8, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{5,4,3,1,2,0,0,0,0,0};
+        assertEquals(6, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{4,2,3,1,0,0,0,0,0,0};
+        assertEquals(5, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{3,2,1,0,0,0,0,0,0,0};
+        assertEquals(4, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{2,1,0,0,0,0,0,0,0,0};
+        assertEquals(3, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{1,0,0,0,0,0,0,0,0,0};
+        assertEquals(2, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+        correctArray = new int[]{0,0,0,0,0,0,0,0,0,0};
+        assertEquals(1, heap.extractMax());
+        assertArrayEquals(correctArray, heap.retrieveArray());
+
+
+    }
+}
